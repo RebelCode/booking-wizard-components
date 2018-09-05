bf28261ccaee5945d4bedd08f7db33d134ea768e
Roman Lytvynenko <litvinenko95@gmail.com>
2018-09-05 09:13:08 +0300

    moved props to one object

--- test/components/CfDateNavigator.spec.js
+++ test/components/CfDateNavigator.spec.js
@@ -20,36 +20,41 @@ const mountDateNavigator = (propsData = {}) => {
   })
 }
 
+/**
+ * Values of DateNavigator props.
+ *
+ * @since [*next-version*]
+ *
+ * @type {{selectedDay: string, prevAvailableDay: string, nextAvailableDay: string}}
+ */
+const dateNavigatorProps = {
+  selectedDay: 'current-day',
+  prevAvailableDay: 'previous-formatted-date',
+  nextAvailableDay: 'next-formatted-date'
+}
+
 describe('CfDateNavigator.js', () => {
   /**
    * Test that date navigator can successfully switch to the next day.
    */
   it('switches forward', () => {
-    const wrapper = mountDateNavigator({
-      selectedDay: 'current-day',
-      prevAvailableDay: 'previous-formatted-date',
-      nextAvailableDay: 'next-formatted-date'
-    })
+    const wrapper = mountDateNavigator(dateNavigatorProps)
 
     wrapper.vm.goToNextDay()
 
     expect(wrapper.emitted()['update:selectedDay']).toBeTruthy()
-    expect(wrapper.emitted()['update:selectedDay'][0]).toEqual(['next-formatted-date'])
+    expect(wrapper.emitted()['update:selectedDay'][0]).toEqual([dateNavigatorProps.nextAvailableDay])
   })
 
   /**
    * Test that date navigator can successfully switch to the previous day.
    */
   it('switches backward', () => {
-    const wrapper = mountDateNavigator({
-      selectedDay: 'current-day',
-      prevAvailableDay: 'previous-formatted-date',
-      nextAvailableDay: 'next-formatted-date'
-    })
+    const wrapper = mountDateNavigator(dateNavigatorProps)
 
     wrapper.vm.goToPrevDay()
 
     expect(wrapper.emitted()['update:selectedDay']).toBeTruthy()
-    expect(wrapper.emitted()['update:selectedDay'][0]).toEqual(['previous-formatted-date'])
+    expect(wrapper.emitted()['update:selectedDay'][0]).toEqual([dateNavigatorProps.prevAvailableDay])
   })
 })
